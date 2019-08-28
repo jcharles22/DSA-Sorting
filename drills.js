87c926a2c58884541106024af08e14666ef7b850
Jared Charles <jaredcharles22@gmail.com>
2019-08-27 20:07:52 -0400

    first commit

--- drills.js
+++ drills.js
@@ -2,47 +2,35 @@ const LinkedList = require("./linkedlist");
 
 // 1. Understanding merge sort
 // Given the following list of numbers 21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40
-
 // What is the resulting list that will be sorted after 3 recursive calls to mergesort?
-//left = mergesort([21, 1, 26, 45, 29, 18, 2, 9])
-//left = mergesort([21, 1, 26, 45])
 // left = mergesort([21, 1])
 
 // What is the resulting list that will be sorted after 16 recursive calls to mergesort?
-
-// right = mergesort([16, 49, 39, 27, 43, 34, 36, 40])
+  // right = mergesort([16, 49, 39, 27, 43, 34, 36, 40])
 
 // What are the first 2 lists to be merged?
-
-//21, 1
+  //21, 1
 
 // Which two lists would be merged on the 7th merge?
+  //[1, 21, 26, 45] and [2, 9, 28, 29]
 
-//[1, 21, 26, 45] and [2, 9, 28, 29]
 
 // 2. Understanding quicksort
 // 1) Suppose you are debugging a quicksort implementation that is supposed to sort an array in ascending order.
 //  After the first partition step has been completed, the contents of the array is in the following order: 3 9 1 14 17 24 22 20.
 //  Which of the following statements is correct about the partition step? Explain your answer.
 
-//a. The pivot could have been 17, but could not have been 14
-//b. The pivot could have been either 14 or 17 <-- true, bc everything to the left of 14 is < 14, and everything to the right of 17 is > 17.
-//c. Neither 14 nor 17 could have been the pivot
-//d. The pivot could have been 14, but could not have been 17
+  //The pivot could have been either 14 or 17 everything to the left of 14 is < 14, and everything to the right of 17 is > 17.
+
 
 // 2) Given the following list of numbers 14, 17, 13, 15, 19, 10, 3, 16, 9, 12
 // show the resulting list after the second partitioning according to the quicksort algorithm.
-
-// When using the last item on the list as a pivot
-//[ 3, 9, 10, 12, 19, 14, 17, 16, 13, 15 ]
-
-// When using the first item on the list as a pivot
-// [14, 13, 10, 3, 9, 12, 15, 16, 19, 17];
+  //[ 3, 9, 10, 12, 19, 14, 17, 16, 13, 15 ]
+  // [14, 13, 10, 3, 9, 12, 15, 16, 19, 17];
 
 // 3. Implementing quicksort
 
 function swap(array, i, j) {
-  //swap function swaps values at two indices
   const tmp = array[i];
   array[i] = array[j];
   array[j] = tmp;
@@ -53,32 +41,23 @@ function qSort(array, start = 0, end = array.length) {
     return array;
   }
 
-  //   console.log(array);
-
-  //   You partition the array into 2 halves around a pivot value.
-  // All of the values which are less than the pivot value go to 1 half of the array,
-  //  and all of the values which are greater than the pivot go to the other half of the array.
-  // You then recursively sort the 2 halves of the array until the halves are of length 0 or 1.
+    // console.log(array);
   const middle = partition(array, start, end);
   array = qSort(array, start, middle);
   array = qSort(array, middle + 1, end);
   return array;
 }
 
-//lumotos algorithm - common in-place partitioning algorithm
 function partition(array, start, end) {
-  // The pivot is the final value in the array.
   const pivot = array[end - 1];
 
   let j = start;
-  //You loop through the array, swapping values as you go to put them on either side of the pivot point.
   for (let i = start; i < end - 1; i++) {
     if (array[i] <= pivot) {
       swap(array, i, j);
       j++;
     }
   }
-  //   And finally, you put the pivot into the correct place in the array.
   swap(array, end - 1, j);
   return j;
 }
@@ -97,19 +76,16 @@ qSort(array);
 //4.  Implementing merge sort
 
 function mSort(array) {
-  //if array has 0 or 1 elements...it is already sorted, so return it
   if (array.length <= 1) {
     return array;
   }
 
-  //cut array in half and sort recursively calling mergeSort
   const middle = Math.floor(array.length / 2);
   let left = array.slice(0, middle);
   let right = array.slice(middle, array.length);
 
   left = mSort(left);
   right = mSort(right);
-  //the two sorted halves are merged together using merge
   return merge(left, right, array);
 }
 
@@ -118,7 +94,6 @@ function merge(left, right, array) {
   let rightIndex = 0;
   let outputIndex = 0;
 
-  //   To merge the 2 lists you just keep choosing the lowest value from the left or right arrays that hasn't already been added to the output array.
   while (leftIndex < left.length && rightIndex < right.length) {
     if (left[leftIndex] < right[rightIndex]) {
       array[outputIndex++] = left[leftIndex++];
@@ -127,7 +102,6 @@ function merge(left, right, array) {
     }
   }
 
-  //   When 1 of the arrays is empty, you add all of the remaining values from the other array to it.
   for (let i = leftIndex; i < left.length; i++) {
     array[outputIndex++] = left[i];
   }
@@ -135,7 +109,7 @@ function merge(left, right, array) {
   for (let i = rightIndex; i < right.length; i++) {
     array[outputIndex++] = right[i];
   }
-  //   console.log("merged:" + array);
+    // console.log("merged:" + array);
   return array;
 }
 
@@ -149,35 +123,93 @@ function main() {
   ll.insertFirst(3);
   ll.insertFirst(9);
   ll.insertFirst(1);
-  console.log(ll);
+  // console.log(ll);
 }
 
 main();
 
-//TODO: finish q 5
-
 //6. Bucket sort
-// Write an O(n) algorithm to sort an array of integers, where you know in advance what the lowest and highest values are.
-// You can't use arr.splice(), shift() or unshift() for this exercise.
 
-function bucketSort(arr, lowest, highest) {
-  //????? - bucket sort usually means splitting array into buckets, then using another algorithm to sort the buckets before merging them again?
-  let firstBucket = [];
-  let secondBucket = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < (highest + lowest) / 2) {
-      firstBucket.push(arr[i]);
-    } else {
-      secondBucket.push(arr[i]);
+function bucketSort(array, bucketSize) {
+      if (array.length === 0) {
+        return array;
+      }
+      var i,
+          minValue = array[0],
+          maxValue = array[0],
+          bucketSize = bucketSize || 5;
+      
+      array.forEach(function (currentVal) {
+          if (currentVal < minValue) {
+              minValue = currentVal;
+          } else if (currentVal > maxValue) {
+              maxValue = currentVal;
+          }
+      })
+    
+      var bucketCount = Math.floor((maxValue - minValue) / bucketSize) + 1;
+      var allBuckets = new Array(bucketCount);
+      
+      for (i = 0; i < allBuckets.length; i++) {
+        allBuckets[i] = [];
+      }
+      
+      array.forEach(function (currentVal) {
+          allBuckets[Math.floor((currentVal - minValue) / bucketSize)].push(currentVal);
+      });
+    
+      array.length = 0;
+      
+      allBuckets.forEach(function(bucket) {
+          insertionSort(bucket);
+          bucket.forEach(function (element) {
+              array.push(element)
+          });
+      });
+    
+      return array;
     }
+
+
+// 7.Sort in place 
+function ranSort(arr) {
+  let temp;
+  let rnd;
+  for(let i = 0; i < arr.length; i++ ) {
+      rnd = Math.floor(Math.random()*arr.length);
+      temp = arr[i];
+      arr[i] = arr[rnd]
+      arr[rnd] = temp
+  }
+  return arr;
+}
+console.log(ranSort([1,2,3,4]))
+
+function SortBooks(arr) {
+  if (arr.length <= 1) {
+    return arr;
   }
 
-  mSort(firstBucket);
-  mSort(secondBucket);
-  console.log(firstBucket.concat(secondBucket));
+  const middle = Math.floor(arr.length / 2);
+  let left = arr.slice(0, middle);
+  let right = arr.slice(middle, arr.length);
+
+  left = SortBooks(left);
+  right = SortBooks(right);
+  return merge(left, right, arr);
 }
 
-bucketSort([32, 1, 82, 26, 24, 99, 3, 2], 1, 99);
-
-//TODO: q 7, q 8
+const books = [
+  'The Great Gatsby',
+  'The Grapes of Wrath',
+  'Nineteen Eighty-Four',
+  'Ulysses',
+  'Harry Potter',
+  'Lolita',
+  'Catch-22',
+  'The Catcher in the Rye',
+  'Beloved',
+  'To Kill a Mockingbird',
+  'The Lord of the Rings'
+];
+console.log(SortBooks(books))
--- linkedlist.js
+++ linkedlist.js
@@ -92,44 +92,32 @@ class LinkedList {
   }
 
   find(item) {
-    // Start at the head
     let currNode = this.head;
-    // If the list is empty
     if (!this.head) {
       return null;
     }
-    // Check for the item
     while (currNode.value !== item) {
-      /* Return null if it's the end of the list 
-                 and the item is not on the list */
       if (currNode.next === null) {
         return null;
       } else {
-        // Otherwise, keep looking
         currNode = currNode.next;
       }
     }
-    // Found it
     return currNode;
   }
 
   remove(item) {
-    // If the list is empty
     if (!this.head) {
       return null;
     }
-    // If the node to be removed is head, make the next node head
     if (this.head.value === item) {
       this.head = this.head.next;
       return;
     }
-    // Start at the head
     let currNode = this.head;
-    // Keep track of previous
     let previousNode = this.head;
 
     while (currNode !== null && currNode.value !== item) {
-      // Save the previous node
       previousNode = currNode;
       currNode = currNode.next;
     }
